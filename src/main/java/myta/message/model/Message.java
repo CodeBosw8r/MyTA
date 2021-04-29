f23227588a749e892bbe727c627cd6fe01c8e1db
erikhoogeboom <bigtree@29a.nl>
2021-04-29 21:22:17 +0200

    added getters and setters in Message

--- src/main/java/myta/message/model/Message.java
+++ src/main/java/myta/message/model/Message.java
@@ -18,4 +18,60 @@ public class Message {
 
     private String             returnPath;
 
+    public List<Recipient> getRecipients() {
+        return recipients;
+    }
+
+    public void setRecipients(List<Recipient> recipients) {
+        this.recipients = recipients;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getHtmlBody() {
+        return htmlBody;
+    }
+
+    public void setHtmlBody(String htmlBody) {
+        this.htmlBody = htmlBody;
+    }
+
+    public String getTextBody() {
+        return textBody;
+    }
+
+    public void setTextBody(String textBody) {
+        this.textBody = textBody;
+    }
+
+    public List<EmailAddress> getReplyTo() {
+        return replyTo;
+    }
+
+    public void setReplyTo(List<EmailAddress> replyTo) {
+        this.replyTo = replyTo;
+    }
+
+    public List<Header> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(List<Header> headers) {
+        this.headers = headers;
+    }
+
+    public String getReturnPath() {
+        return returnPath;
+    }
+
+    public void setReturnPath(String returnPath) {
+        this.returnPath = returnPath;
+    }
+
 }

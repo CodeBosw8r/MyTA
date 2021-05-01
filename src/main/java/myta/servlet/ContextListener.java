bb6ab533319bc7e29211a2c248ed6ccc97622081
erikhoogeboom <bigtree@29a.nl>
2021-05-01 10:59:08 +0200

    implemented ContextListener

--- src/main/java/myta/servlet/ContextListener.java
+++ src/main/java/myta/servlet/ContextListener.java
@@ -1,13 +1,23 @@
 package myta.servlet;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+
+import myta.service.IncomingMessageQueueManager;
 
 public class ContextListener implements ServletContextListener {
 
     public void contextDestroyed(ServletContextEvent arg0) {
     }
 
-    public void contextInitialized(ServletContextEvent arg0) {
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+
+        ServletContext servletContext = servletContextEvent.getServletContext();
+
+        IncomingMessageQueueManager incomingMessageQueueManager = new IncomingMessageQueueManager();
+
+        servletContext.setAttribute("incomingMessageQueueManager", incomingMessageQueueManager);
+
     }
 }

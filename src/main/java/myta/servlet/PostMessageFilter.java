62d53f6baac805a99a2a34e4e47e6c1697ebaa5a
erikhoogeboom <bigtree@29a.nl>
2021-05-01 11:00:29 +0200

    PostMessageFilter now parses json request body

--- src/main/java/myta/servlet/PostMessageFilter.java
+++ src/main/java/myta/servlet/PostMessageFilter.java
@@ -6,17 +6,29 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Writer;
 import java.nio.charset.Charset;
+import java.util.Map;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import myta.message.model.Message;
+import myta.service.IncomingMessageQueueManager;
+
 public class PostMessageFilter implements Filter {
 
+    private IncomingMessageQueueManager incomingMessageQueueManager;
+
+    @SuppressWarnings("unchecked")
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
         // TODO Auto-generated method stub
@@ -34,6 +46,43 @@ public class PostMessageFilter implements Filter {
             Object requestObject = this.parseJsonRequestObject(httpServletRequest);
 
             if (requestObject != null) {
+
+                if (requestObject instanceof Map) {
+
+                    Map<String, Object> requestMap = null;
+
+                    requestMap = (Map<String, Object>) requestObject;
+
+                    if (requestMap != null) {
+
+                        Message parsedMessage = null;
+
+                        try {
+
+                            parsedMessage = this.parseMessage(requestMap);
+
+                        } catch (Exception e) {
+
+                            // show error
+
+                        }
+
+                        if (parsedMessage != null) {
+
+                            // enqueue message
+                            IncomingMessageQueueManager incomingMessageQueueManager = this.getIncomingMessageQueueManager();
+
+                            if (incomingMessageQueueManager != null) {
+
+                                incomingMessageQueueManager.enqueueMessage(parsedMessage);
+
+                            }
+
+                        }
+
+                    }
+
+                }
 
             } else {
 
@@ -107,20 +156,73 @@ public class PostMessageFilter implements Filter {
     }
 
     @Override
-    public void init(FilterConfig var1) throws ServletException {
+    public void init(FilterConfig filterConfig) throws ServletException {
         // TODO Auto-generated method stub
+
+        ServletContext servletContext = filterConfig.getServletContext();
+        Object incomingMessageQueueManagerObject = servletContext.getAttribute("incomingMessageQueueManager");
+
+        if (incomingMessageQueueManagerObject != null) {
+
+            if (incomingMessageQueueManagerObject instanceof IncomingMessageQueueManager) {
+
+                this.incomingMessageQueueManager = (IncomingMessageQueueManager) incomingMessageQueueManagerObject;
+
+            }
+
+        }
 
     }
 
-    public Object parseJsonRequestObject(HttpServletRequest httpServletRequest) {
+    public Object parseJsonRequestObject(HttpServletRequest httpServletRequest) throws IOException {
 
         Object obj = null;
 
-        if (httpServletRequest.getContentType().equals("application/json")) {
+        String contentType = httpServletRequest.getContentType();
+
+        if ((contentType != null) && contentType.toLowerCase().startsWith("application/json")) {
+
+            InputStream inputStream = httpServletRequest.getInputStream();
+
+            obj = this.parseJsonRequestBody(inputStream);
 
         }
 
         return obj;
+
+    }
+
+    public Object parseJsonRequestBody(InputStream inputStream) throws JsonParseException, JsonMappingException, IOException {
+
+        Object obj = null;
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        obj = mapper.readValue(inputStream, Object.class);
+
+        return obj;
+
+    }
+
+    public Message parseMessage(Map<String, Object> objectMap) throws Exception {
+
+        Message message = null;
+
+        if (objectMap != null) {
+
+            // check required fields
+
+            // check valid email addresses
+
+        }
+
+        return message;
+
+    }
+
+    public IncomingMessageQueueManager getIncomingMessageQueueManager() {
+
+        return this.incomingMessageQueueManager;
 
     }
 

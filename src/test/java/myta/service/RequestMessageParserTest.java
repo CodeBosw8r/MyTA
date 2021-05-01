bdff924e5550e2d8cc0ce31f13f2689233290902
Erik Hoogeboom <bigtree@29a.nl>
2021-05-01 13:58:49 +0200

    added empty test

---
+++ src/test/java/myta/service/RequestMessageParserTest.java
@@ -0,0 +1,16 @@
+package myta.service;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class RequestMessageParserTest {
+
+    @Test
+    public void testParseSimpleValidMessage() {
+
+        fail("Not implemented");
+
+    }
+
+}

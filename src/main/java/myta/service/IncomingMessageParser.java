9eabaddd811064e7cdbd34305ebbba1abdd4b19e
erikhoogeboom <bigtree@29a.nl>
2021-05-02 14:44:40 +0200

    implemented stub IncomingMessageParser

---
+++ src/main/java/myta/service/IncomingMessageParser.java
@@ -0,0 +1,16 @@
+package myta.service;
+
+import java.util.Map;
+
+import myta.exception.MessageParseException;
+import myta.message.model.Message;
+
+public class IncomingMessageParser {
+
+    public Message parseMessageRequest(Map<String, Object> requestMap) throws MessageParseException {
+
+        return null;
+
+    }
+
+}

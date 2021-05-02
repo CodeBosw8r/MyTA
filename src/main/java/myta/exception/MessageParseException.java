5b57a60f6157a1d65e335c304737be3ee264ec96
erikhoogeboom <bigtree@29a.nl>
2021-05-02 16:12:21 +0200

    fixed MessageParseException constructor

--- src/main/java/myta/exception/MessageParseException.java
+++ src/main/java/myta/exception/MessageParseException.java
@@ -2,6 +2,10 @@ package myta.exception;
 
 public class MessageParseException extends Exception {
 
+    public MessageParseException(String message) {
+        super(message);
+    }
+
     private static final long serialVersionUID = 4535667593609534926L;
 
 }

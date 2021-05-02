016ca5d9683ac8f5db9f5b004a238827176eb139
erikhoogeboom <bigtree@29a.nl>
2021-05-02 14:46:42 +0200

    implemented IncomingMessageQueueManager

--- src/main/java/myta/service/IncomingMessageQueueManager.java
+++ src/main/java/myta/service/IncomingMessageQueueManager.java
@@ -1,10 +1,35 @@
 package myta.service;
 
-import myta.message.model.Message;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+import myta.queue.model.IncomingMessageQueueEntry;
 
 public class IncomingMessageQueueManager {
 
-    public void enqueueMessage(Message message) {
+    private final BlockingQueue<IncomingMessageQueueEntry> queue;
+
+    public IncomingMessageQueueManager(int queueSize) {
+
+        this.queue = new ArrayBlockingQueue<IncomingMessageQueueEntry>(queueSize, true);
+
+    }
+
+    public void enqueueMessage(IncomingMessageQueueEntry entry) {
+
+        this.queue.add(entry);
+
+    }
+
+    public IncomingMessageQueueEntry takeQueueEntry() throws InterruptedException {
+
+        return queue.take();
+
+    }
+
+    public int getQueueSize() {
+
+        return this.queue.size();
 
     }
 

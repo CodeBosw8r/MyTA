669d3e3c2f7dfbecfb719cdfe8e232c6ffdcbf3c
erikhoogeboom <bigtree@29a.nl>
2021-05-05 19:58:14 +0200

    ContextListener now creates Engine and no IncomingMessageQueueManager

--- src/main/java/myta/servlet/ContextListener.java
+++ src/main/java/myta/servlet/ContextListener.java
@@ -4,22 +4,33 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import myta.service.IncomingMessageQueueManager;
+import myta.core.Engine;
 
 public class ContextListener implements ServletContextListener {
 
-    public void contextDestroyed(ServletContextEvent arg0) {
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+        ServletContext servletContext = servletContextEvent.getServletContext();
+
+        Object engineObject = servletContext.getAttribute("engine");
+
+        if ((engineObject != null) && (engineObject instanceof Engine)) {
+
+            Engine engine = (Engine) engineObject;
+            engine.shutdown();
+
+        }
+
     }
 
     public void contextInitialized(ServletContextEvent servletContextEvent) {
 
         ServletContext servletContext = servletContextEvent.getServletContext();
 
-        int queueSize = 1000;
+        Engine engine = new Engine();
+        engine.initialize();
 
-        IncomingMessageQueueManager incomingMessageQueueManager = new IncomingMessageQueueManager(queueSize);
-
-        servletContext.setAttribute("incomingMessageQueueManager", incomingMessageQueueManager);
+        servletContext.setAttribute("engine", engine);
 
     }
 }

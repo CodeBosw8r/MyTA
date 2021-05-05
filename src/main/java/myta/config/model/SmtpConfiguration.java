dc8a311fa8e7b6a27c2b38391e9205f5f01d6bc7
erikhoogeboom <bigtree@29a.nl>
2021-05-05 19:56:26 +0200

    implemented SmtpConfiguration model class

---
+++ src/main/java/myta/config/model/SmtpConfiguration.java
@@ -0,0 +1,15 @@
+package myta.config.model;
+
+public class SmtpConfiguration {
+
+    private String host;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+}

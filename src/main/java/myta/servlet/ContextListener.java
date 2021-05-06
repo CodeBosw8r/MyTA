d90f4d5f2fb8993b70723501eee0d71dd08a367f
erikhoogeboom <bigtree@29a.nl>
2021-05-06 20:02:38 +0200

    ContextListener now creates EngineConfig to pass to Engine init

--- src/main/java/myta/servlet/ContextListener.java
+++ src/main/java/myta/servlet/ContextListener.java
@@ -1,12 +1,15 @@
 package myta.servlet;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import myta.config.model.EngineConfig;
+import myta.config.model.SmtpConfiguration;
 import myta.core.Engine;
 
 public class ContextListener implements ServletContextListener {
@@ -30,49 +33,61 @@ public class ContextListener implements ServletContextListener {
 
         ServletContext servletContext = servletContextEvent.getServletContext();
 
-        Map<String, String> params = new HashMap<String, String>();
+        EngineConfig engineConfig = new EngineConfig();
 
         Map<String, String> env = System.getenv();
 
-        if (env.containsKey("workers") && (env.get("workers") != null) && env.get("workers").toString().matches("[1-9]{1}[0-9]{0,}")) {
+        if (env.containsKey("NUM_WORKERS") && (env.get("NUM_WORKERS") != null)) {
 
-            params.put("numWorkers", env.get("workers").toString());
+            if (env.get("NUM_WORKERS").toString().matches("[1-9]{1}[0-9]{0,}")) {
 
-        }
+                engineConfig.setNumWorkers(Integer.valueOf(env.get("NUM_WORKERS")));
 
-        if (env.containsKey("NUM_WORKERS") && (env.get("NUM_WORKERS") != null) && env.get("NUM_WORKERS").toString().matches("[1-9]{1}[0-9]{0,}")) {
-
-            params.put("numWorkers", env.get("NUM_WORKERS").toString());
+            }
 
         }
 
-        if (env.containsKey("queuesize") && (env.get("queuesize") != null) && env.get("queuesize").toString().matches("[1-9]{1}[0-9]{0,}")) {
+        if (env.containsKey("INCOMING_QUEUE_SIZE") && (env.get("INCOMING_QUEUE_SIZE") != null)) {
 
-            params.put("queueSize", env.get("queuesize").toString());
+            if (env.get("INCOMING_QUEUE_SIZE").toString().matches("[1-9]{1}[0-9]{0,}")) {
 
-        }
+                engineConfig.setIncomingMessageQueueSize(Integer.valueOf(env.get("INCOMING_QUEUE_SIZE")));
 
-        if (env.containsKey("INCOMING_QUEUE_SIZE") && (env.get("INCOMING_QUEUE_SIZE") != null)
-                && env.get("INCOMING_QUEUE_SIZE").toString().matches("[1-9]{1}[0-9]{0,}")) {
-
-            params.put("queueSize", env.get("INCOMING_QUEUE_SIZE").toString());
-
-        }
-
-        if (env.containsKey("relayhost") && (env.get("relayhost") != null) && !env.get("relayhost").equals("")) {
-
-            params.put("relayHost", env.get("relayhost").toString());
+            }
 
         }
 
         if (env.containsKey("RELAY_HOST") && (env.get("RELAY_HOST") != null) && !env.get("RELAY_HOST").equals("")) {
 
-            params.put("relayHost", env.get("RELAY_HOST").toString());
+            String servers[] = env.get("RELAY_HOST").replaceAll(" ", "").split(",");
+
+            List<SmtpConfiguration> relayServers = new ArrayList<SmtpConfiguration>(servers.length);
+
+            for (String server : servers) {
+
+                server = server.trim();
+
+                if (!server.equals("")) {
+
+                    String[] serverParts = server.split(":");
+
+                    String serverName = serverParts[0];
+
+                    SmtpConfiguration relayServer = new SmtpConfiguration(serverName);
+                    relayServers.add(relayServer);
+
+                }
+
+            }
+
+            if (relayServers.size() > 0) {
+                engineConfig.setRelayServers(relayServers);
+            }
 
         }
 
         Engine engine = new Engine();
-        engine.initialize(params);
+        engine.initialize(engineConfig);
 
         servletContext.setAttribute("engine", engine);
 

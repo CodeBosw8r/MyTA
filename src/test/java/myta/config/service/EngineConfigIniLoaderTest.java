1c07abbe311409c050b794ac1ff79c60edd2285a
erikhoogeboom <bigtree@29a.nl>
2021-05-11 19:48:36 +0200

    fixed imports in EngineConfigIniLoaderTest

--- src/test/java/myta/config/service/EngineConfigIniLoaderTest.java
+++ src/test/java/myta/config/service/EngineConfigIniLoaderTest.java
@@ -1,8 +1,8 @@
 package myta.config.service;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.FileInputStream;
